cc764bc2afadb5922db54c55d51f4f83ac309bfe
Yotaru1250 <50357041+Yotaru1250@users.noreply.github.com>
2019-05-14 02:23:34 +0900

    Add files via upload

---
+++ code/NextGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NextGame : MonoBehaviour {
+	[SerializeField]
+	private GameObject pauseUI;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick() {
+
+	if(pauseUI.activeSelf) {
+      pauseUI.SetActive (!pauseUI.activeSelf);
+    	Time.timeScale = 1f;
+	 }
+	}
+
+}
---
+++ code/Pause.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Pause : MonoBehaviour {
+ 
+	//　ポーズした時に表示するUI
+	[SerializeField]
+	private GameObject pauseUI;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		Cursor.lockState = CursorLockMode.Confined;
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			//　ポーズUIのアクティブ、非アクティブを切り替え
+			pauseUI.SetActive (!pauseUI.activeSelf);
+			
+			
+ 
+			//　ポーズUIが表示されてる時は停止
+			if(pauseUI.activeSelf) {
+				Time.timeScale = 0f;	//時間停止
+				Cursor.visible = true;		//カーソル表示
+				Cursor.lockState = CursorLockMode.None;//カーソルのロック解除
+				Cursor.lockState = CursorLockMode.Confined;//カーソルをウィンドウ内に
+			//　ポーズUIが表示されてなければ通常通り進行
+			} 
+			else {
+				Time.timeScale = 1f;	//時間再開
+				Cursor.lockState = CursorLockMode.Confined;//カーソルをウィンドウ内に
+			}
+		}
+	}
+
+
+	
+}
