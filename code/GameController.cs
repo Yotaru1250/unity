dabaa53849c5ff973ea8c024f99981202da3e577
Yotaru1250 <50357041+Yotaru1250@users.noreply.github.com>
2019-06-28 18:04:37 +0900

    Add files via upload

---
+++ code/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public GameObject GoalText;
+	// Use this for initialization
+	void Start () {
+		GoalText.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnTriggerEnter(Collider collider){
+
+		if(collider.gameObject.tag == "Player"){
+			 StartCoroutine(Example());
+		}
+			
+	}
+	
+
+	IEnumerator Example()
+    {
+        
+		GoalText.SetActive(true);
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("Menu");
+		
+    }
+
+
+}
---
+++ code/Gamecursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamecursor: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// カーソル非表示
+		Cursor.visible = false;
+	
+	}
+}
